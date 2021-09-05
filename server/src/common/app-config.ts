49f577a68adab5a2536f4f008115d40a3d6c843a
Dany Boza <db@backium.co>
2021-09-05 17:34:19 -0500

    add image preview for template #104

--- server/src/common/app-config.ts
+++ server/src/common/app-config.ts
@@ -4,13 +4,28 @@ import dotenv from "dotenv";
 dotenv.config();
 
 class Config implements AppConfig {
-  public mongoDbConnString: string;
-  public mongoDbName: string;
+  public mongo: { connString: string; name: string };
+  public aws: {
+    cloudfrontAddress: string;
+    bucket: string;
+    credentials: { accessKeyId: string; secretAccessKey: string };
+  };
   public appPort: number;
   public appHost: string;
   constructor() {
-    this.mongoDbConnString = process.env.MONGO_DB_CONN_STRING as string;
-    this.mongoDbName = process.env.MONGO_DB_NAME as string;
+    this.mongo = {
+      name: process.env.MONGO_DB_NAME as string,
+      connString: process.env.MONGO_DB_CONN_STRING as string,
+    };
+    this.aws = {
+      cloudfrontAddress: process.env.AWS_CLOUDFRONT as string,
+
+      bucket: process.env.AWS_BUCKET_NAME as string,
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+      },
+    };
     this.appPort = parseInt((process.env.PORT as string) || "8080");
   }
 }
